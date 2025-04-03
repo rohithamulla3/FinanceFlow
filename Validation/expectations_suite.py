c48f739c827f91bcd89a981cf489c3eb5e361fea
rohithamulla3 <rohithamulla3@gmail.com>
2025-04-03 16:29:45 -0400

    Add files via upload

--- Validation/expectations_suite.py
+++ Validation/expectations_suite.py
@@ -1,1 +1,13 @@
+from great_expectations.dataset import PandasDataset
+import pandas as pd
+from utils.logger import get_logger
 
+logger = get_logger("DataValidation")
+
+def validate_data():
+    logger.info("Validating data with Great Expectations...")
+    df = pd.read_csv('/tmp/generated_transactions.csv')
+    dataset = PandasDataset(df)
+    assert dataset.expect_column_values_to_not_be_null('transaction_id').success
+    assert dataset.expect_column_values_to_be_between('amount', min_value=1, max_value=10000).success
+    logger.info("Data validation passed.")
