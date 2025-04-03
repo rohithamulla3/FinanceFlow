374439e2058e011f4a8727d73cd4a691c46beea2
rohithamulla3 <rohithamulla3@gmail.com>
2025-04-03 16:27:33 -0400

    Add files via upload

--- Lambda/transform_function.py
+++ Lambda/transform_function.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
+from utils.logger import get_logger
 
+logger = get_logger("LambdaTransform")
+
+def lambda_transform():
+    logger.info("Transforming data in Lambda simulation...")
+    df = pd.read_csv('/tmp/generated_transactions.csv')
+    df['amount_usd'] = df['amount'] * np.random.uniform(0.9, 1.1, len(df))  # Simulate FX
+    df.to_csv('/tmp/transformed_transactions.csv', index=False)
+    logger.info("Transformed data saved to /tmp/transformed_transactions.csv")
